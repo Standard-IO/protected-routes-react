fed3cf6d688beb85d043c91489f21486af99172f
standardio <israelrescalvo@outlook.com>
2023-03-15 13:33:10 -0600

    Basic routing added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom"
+import { Home, Dasboard, Admin, Analytics } from "./routes/pages"
+export default function App() {
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <BrowserRouter>
+      <h1>Welcome!</h1>
+      <Navbar />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='dashboard/' element={<Dasboard />} />
+        <Route path='Admin/' element={<Admin />} />
+        <Route path='Analytics/' element={<Analytics />} />
+      </Routes>
+    </BrowserRouter>
+
   )
 }
 
-export default App
+
+function Navbar() {
+  return (
+    <ul>
+      <li>
+        <Link className="element" to='/'>Home</Link>
+      </li>
+      <li>
+        <Link className="element" to='dashboard/'>Dasboard</Link>
+      </li>
+      <li>
+        <Link className="element" to='analytics/'>Analytics</Link>
+      </li>
+      <li>
+        <Link className="element" to='admin/'>Admin</Link>
+      </li>
+    </ul>
+  )
+}
---
+++ frontend/src/routes/pages.jsx
@@ -0,0 +1,22 @@
+export function Home(){
+    return(
+        <h1>Home</h1>
+    )
+}
+
+export function Dasboard(){
+    return(
+        <h1>Dashboard</h1>
+    )
+}
+export function Analytics(){
+    return(
+        <h1>Analitics</h1>
+    )
+}
+
+export function Admin(){
+    return(
+        <h1>Admin</h1>
+    )
+}
